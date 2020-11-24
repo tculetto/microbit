b0e9d8b5ec3a614bf9324f49cb6837dcdcc6fc10
Thomas Culetto <50497825+tculetto@users.noreply.github.com>
2020-11-24 16:41:46 +0000

    test

--- main.ts
+++ main.ts
@@ -1,9 +1,6 @@
-let test = 0
 basic.forever(function () {
     basic.pause(1000)
     basic.showIcon(IconNames.SmallHeart)
     basic.pause(1000)
     basic.showIcon(IconNames.Heart)
-    test = 0
-    basic.pause(test + 4)
 })

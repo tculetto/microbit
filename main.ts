a62cf9972420908c5e315f3a98e688ccc8d64e2a
Thomas Culetto <50497825+tculetto@users.noreply.github.com>
2020-12-07 11:51:39 +0000

    clean up

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ basic.pause(2000)
 let times_table_number = 9
 basic.pause(200)
 basic.showNumber(times_table_number)
-for (let index = 0; index < 10; index++) {
+for (let index = 0; index < 12; index++) {
     basic.pause(500)
     basic.showNumber(times_table_number)
     times_table_number += 9
